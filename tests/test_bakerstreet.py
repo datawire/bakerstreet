00e7cbc939dd657f4fe1e535d6fbb18853b63323
Philip Lombardi <plombardi@datawire.io>
2015-09-25 13:21:39 -0400

    Fixed test_bakerstreet.py to actually perform assertions.
    Tests with actual assertions that I forgot earlier after debugging.

--- tests/test_bakerstreet.py
+++ tests/test_bakerstreet.py
@@ -7,14 +7,14 @@ def test_resolve_service_with_path(wc_host):
 
     load_watson_with_config(wc_host, "watson-test_service-path.conf")
     time.sleep(5)
-    requests.get("http://localhost:8000/foobar", params=dict(name="Homer")) == "Hi, Homer!"
+    assert requests.get("http://localhost:8000/foo", params=dict(name="Homer")).text == "Hi, Homer!"
 
 def test_resolve_service_without_path(wc_host):
     """Test that Baker Street will route traffic properly"""
 
     load_watson_with_config(wc_host, "watson-test_service-nopath.conf")
     time.sleep(5)
-    requests.get("http://localhost:8000/foobar", params=dict(name="Homer")) == "Hi, everybody!"
+    assert requests.get("http://localhost:8000/bar", params=dict(name="Homer")).text == "Hi, everybody!"
 
 def load_watson_with_config(wc_host, config_name):
     resp = requests.post("http://%s/watsons" % wc_host, params=dict(config_name=config_name))
