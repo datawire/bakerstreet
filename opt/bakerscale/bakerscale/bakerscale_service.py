bcf409c5e423b72f5ea9084e31d5c6d365554de1
Philip Lombardi <plombardi@datawire.io>
2015-10-07 13:43:13 -0400

    Add unique id and request index to the bakerscale_service for tracking

--- opt/bakerscale/bakerscale/bakerscale_service.py
+++ opt/bakerscale/bakerscale/bakerscale_service.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python
 
 import sys
+import threading
+import uuid
 
-from flask import Flask, request
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+index = 0
+service_id = uuid.uuid4()
+
+def synchronized(func):
+    func.__lock__ = threading.Lock()
+
+    def synchronized_func(*args, **kwargs):
+        with func.__lock__:
+            return func(*args, **kwargs)
+
+    return synchronized_func
+
+@synchronized
+def increment_index():
+    global index
+    index += 1
+
 @app.route("/")
-def hello_generic():
-    return "Hi, everybody!"
+def hello():
+    increment_index()
+    return jsonify(service_id=str(service_id), index=index, message="Hello, world!")
 
 @app.route("/health")
 def health_check():
-    return "Healthy!"
-
-@app.route("/hello")
-def hello_name():
-    name = request.args.get('name')
-    return "Hi, %s!" % name
+    increment_index()
+    return jsonify(service_id=str(service_id), index=index, status='OK')
 
 if __name__ == "__main__":
-    args = sys.argv
-    app.run(port=int(args[1]), host="0.0.0.0")
+    passed_args = sys.argv
+    app.run(port=int(passed_args[1]), host="0.0.0.0")
